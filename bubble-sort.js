57653150d7297f0d480cd491530733422835d3da
tishanW98 <shakilatishan1998@outlook.com>
2024-04-02 20:05:56 +0530

    bubble-sort-added

---
+++ bubble-sort.js
@@ -0,0 +1,21 @@
+arr = [-2, 10, 4, -6, 8, 1];
+
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+bubbleSort(arr);
+console.log(arr);
+
+//O(n^2) two nested loops
